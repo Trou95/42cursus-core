4980e7385fd24cc9b8d1dcc7c2bcfab9b459d81e
Gorkem95 <gorkem_095@hotmail.com>
2022-06-12 03:44:11 +0300

    Update Return Type And Error

--- Pushswap/ft_checknumber.c
+++ Pushswap/ft_checknumber.c
@@ -5,40 +5,50 @@ void ft_check_numbers(int ac, char** av)
 {
 	int i;
 	int index;
+	int bIsDigit;
+	int number_count;
 
 	i = 0;
+	number_count = 0;
 	while (i < ac)
 	{
 		index = 0;
 		while (av[i][index])
-			index = ft_check_number(av[i][index],0,0,0);
+		{
+			bIsDigit = 0;
+			ft_check_number(av[i][index], &index, &bIsDigit, 0);
+			if (bIsDigit)
+				number_count++;
+		}
 		i++;
 	}
 }
 
-int ft_check_number(const char* str,int i,int bIsDigit,int bIsSign)
+int ft_check_number(const char* str,int* i,int* bIsDigit,int type)
 {
 	int n;
 	long long	val;
+	int	bIsSign;
 
-	while (str[i] && str[i] == ' ')
-		i++;
-	n = i;
-	if (str[i] == '+' || str[i] == '-')
+	bIsSign = 0;
+	while (str[*i] && str[*i] == ' ')
+		*i++;
+	n = *i;
+	if (str[*i] == '+' || str[*i] == '-')
 	{
-		i++;
+		*i++;
 		bIsSign = 1;
 	}
-	while (str[i] && str[i] != ' ')
+	while (str[*i] && str[*i] != ' ')
 	{
-		if (!ft_isdigit(str[i++]))
+		if (!ft_isdigit(str[*i++]))
 			return ft_util_msg(NULL);
-		bIsDigit = 1;
+		*bIsDigit = 1;
 	}
-	if (!bIsDigit && bIsSign)
+	if (!*bIsDigit && bIsSign)
 		return ft_util_msg(NULL);
+	if (type == 0)
+		return 0;
 	val = ft_atoi(str + n);
-	if (val > INT_MAX || val < INT_MIN)
-		return ft_util_msg(NULL);
-	return (i);
+	return (val);
 }
--- Pushswap/utils/ft_atoi.c
+++ Pushswap/utils/ft_atoi.c
@@ -13,5 +13,8 @@ long long ft_atoi(const char* str)
 			n = -1;
 	while (*str && ft_isdigit(*str))
 		val = val * 10 + *str++ - 48;
-	return (val * n);
+	val = val * sign;
+	if (val > INT_MAX || val < INT_MIN)
+		return ft_util_msg(NULL);
+	return (val);
 }
