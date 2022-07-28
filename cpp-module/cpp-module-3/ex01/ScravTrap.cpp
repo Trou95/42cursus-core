b5ca96775280d131391374ab2f8c138cf4b291dd
Gorkem95 <gorkem_095@hotmail.com>
2022-07-29 00:28:33 +0300

    Add ex01

---
+++ cpp-module/cpp-module-3/ex01/ScravTrap.cpp
@@ -0,0 +1,35 @@
+#include "ScravTrap.h"
+
+ScravTrap::ScravTrap() : ScravTrap("Player 1") {
+
+}
+
+ScravTrap::ScravTrap(const string& name) : ClapTrap(name)
+{
+	_name = name;
+	_hitpoints = 100;
+	_energy = 50;
+	_attackdamage = 20;
+	cout << "ScravTrap " << _name << " created" << std::endl;
+}
+
+ScravTrap::ScravTrap(const ScravTrap& scravtrap) {
+	*this = scravtrap;
+}
+
+ScravTrap& ScravTrap::operator=(const ScravTrap& scravtrap) {
+	this->_name = scravtrap._name;
+	this->_hitpoints = scravtrap._hitpoints;
+	this->_energy = scravtrap._energy;
+	this->_attackdamage = scravtrap._attackdamage;
+	return *this;
+}
+
+void ScravTrap::guardGate() {
+	cout << "ScravTrap " << _name << " now in gate keepermode" << std::endl;
+}
+
+ScravTrap::~ScravTrap()
+{
+	cout << "Scravtrap " << _name << " destroy" << std::endl;
+}
---
+++ cpp-module/cpp-module-3/ex01/main.cpp
@@ -0,0 +1,7 @@
+#include "ScravTrap.h"
+
+int main(int ac, char**av)
+{
+	ScravTrap scravtrap("Player 2");
+	scravtrap.guardGate();
+}
