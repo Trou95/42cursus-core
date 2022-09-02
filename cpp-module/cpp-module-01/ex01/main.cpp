a3805ada95955438231bae77f072081a5488f3e2
Gorkem95 <gorkem_095@hotmail.com>
2022-09-02 03:44:48 +0300

    Added modules

---
+++ cpp-module/cpp-module-01/ex01/main.cpp
@@ -0,0 +1,15 @@
+#include "Zombie.h"
+
+int main()
+{
+    Zombie zombie;
+    Zombie* zombies;
+
+    zombies = zombie.zombieHorde(3, "Zombie");
+
+    for (int i = 0; i < 3; ++i) {
+        zombies[i].announce();
+    }
+
+    delete[] zombies;
+}
